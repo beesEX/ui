bae62a630d89900240727972706401542ddcfe30
Vu Duc Tung <tunggad.vu@googlemail.com>
2018-07-19 14:41:35 +0200

    order-replay page starts to process order place events from gemini

--- public/js/order-replay/index.js
+++ public/js/order-replay/index.js
@@ -1,16 +1,24 @@
 let geminiWS;
+let selectedSide;
+let orderList;
 
-function processWSEvent(msg) {
-  if (msg.type === 'update') {
-    for (let i = 0; i < msg.events.length; i++) {
-      const event = msg.events[i];
-      if (event.type === 'change') {
-      }
-    }
+function toogleConnection() {
+  const btn = document.getElementById('controlBtn');
+  if (btn.innerHTML === 'connect') {
+    connectWS();
+    btn.innerHTML = 'disconnect';
+  } else {
+    closeWS();
+    btn.innerHTML = 'connect';
   }
 }
 
 function connectWS() {
+  const sideSelectEle = document.getElementById('side');
+  selectedSide = sideSelectEle.options[sideSelectEle.selectedIndex].value;
+
+  orderList = document.getElementById('orderList');
+
   geminiWS = new WebSocket('wss://api.gemini.com/v1/marketdata/btcusd');
   console.log(`readyState=${geminiWS.readyState}`);
 
@@ -19,6 +27,26 @@ function connectWS() {
     console.log(JSON.stringify(msg, null, 2));
     processWSEvent(msg);
   };
+}
+
+
+function processWSEvent(msg) {
+  if (msg.type === 'update') {
+    for (let i = 0; i < msg.events.length; i++) {
+      const event = msg.events[i];
+      if (event.type === 'change' & event.reason === 'place') {
+        processOrderPlacedEvent(event);
+      }
+    }
+  }
+}
+
+function processOrderPlacedEvent(orderEvent) {
+  if (orderEvent.side === selectedSide) {
+    const li = document.createElement('li');
+    li.appendChild(document.createTextNode(`${orderEvent.side} ${orderEvent.price} ${orderEvent.delta}`));
+    orderList.insertBefore(li, orderList.firstChild);
+  }
 }
 
 function closeWS() {
