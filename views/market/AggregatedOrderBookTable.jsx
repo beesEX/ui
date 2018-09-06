9e7d86aa4092980b94b9d4c93bb76299d4cadfbe
Vu Duc Tung <tunggad.vu@googlemail.com>
2018-09-06 17:27:21 +0200

    #5: peer review of Lam Hoang work part2

--- views/market/AggregatedOrderBookTable.jsx
+++ views/market/AggregatedOrderBookTable.jsx
@@ -30,7 +30,7 @@ export default class AggregatedOrderBookTable extends React.PureComponent {
       const parsedData = JSON.parse(event.data);
       console.log(parsedData);
       if (parsedData.type === 'ORDER_BOOK_EVENT' ) {
-        const { matches, reason: {type, side, price, quantity, oldQuantity }, filledCompletely } = JSON.parse(event.data);
+        const { matches, reason: {type, side, price, quantity, oldQuantity }, filledCompletely } = JSON.parse(event.data); // [Tung]: also destruct 'oldPrice' and 'filledQuantity' fields of reason order
         let tradedQuantity = 0;
         // if matches then check/update also the other side
         if (matches.length) {
@@ -38,7 +38,16 @@ export default class AggregatedOrderBookTable extends React.PureComponent {
         }
         const _side = this.getSide(side); // [Tung]: '_side' var should be renamed to 'sideVolumes' or 'sideState'. Naming the two vars 'side' and '_side' is very confusing, they are different things
         const index = this.getByPrice( _side, price );
-        if (filledCompletely) this.removeVolumeByPrice(_side, index); // [Tung]: filledCompletely=true means, the the volumes at that price level should be decreased by quantity, but does not mean, that the volumes at that price level should be removed completely like this.
+        /*
+        [Tung]: i would restructure the if-else-if block so that the switch(type)-block proceeds first at top level of the logic structure, no complex wrapping if-else-if anymore.
+        - fillCompletely === true should only be handled in cases 'PLACED' and 'UPDATED'.
+        - index existence check should only be done in cases 'PLACED' and 'UPDATED' and only when fillCompletely !== true is.
+        - case 'PLACED': if (fillCompletely === true) then do no thing, because the order being placed doesn't exist on aggregated book it matches immediately. If fillCompletely !== true, just increase quantity-volume by quantity and filledQuantity-volume by SUM-of-tradedQuantity-of-matches at price level of reason order
+        - case 'UPDATED': first check, whether there was a price change (oldPrice !== price), if there was no price change (oldPrice === price) just update (increase or decrease) quantity-volume at price level of the reason order, compare quantity and oldQuantity to know whether to increase or decrease volume
+        If there was price change (oldPrice !== price) then first decrease quantity-volume by oldQuantity and filledQuantity-volume by filledQuantity at oldPrice level of reason order AND if (fillCompletely === true) then do nothing, else increase quantity-volume by quantity and filledQuantity-volume by filledQuantity at price level of reason order.
+        - case 'CANCELED': just decrease the quantity-volume by quantity and filledQuantity-volume by filledQuantity at the price level of the reason order
+         */
+        if (filledCompletely) this.removeVolumeByPrice(_side, index); // [Tung]: filledCompletely=true means, the the volumes (quantity and filled) at that price level should be decreased by quantity of the reason, but does not mean, that the volumes at that price level should be removed completely like this.
         else if ( typeof index === "number" ) {
           switch (type) {
             case 'PLACED':
@@ -61,13 +70,17 @@ export default class AggregatedOrderBookTable extends React.PureComponent {
     };
   }
 
+  /*
+  [Tung]: typo error: the function should be named 'processMatches' right?
+  In this function you also have to check the property 'filledCompletely' === true/false to know whether it is necessary to decrease volumes (quantity, filledQuantity) at price level of match order on counter side.
+   */
   progressMatches = (side, matches) => {
     const _side = side === 'SELL' ? 'BUY' : 'SELL'; // [Tung]: '_side' var should be renamed to 'counterSide'
     const _otherSide = this.getSide( _side ); // [Tung]: '_otherSide' var should be renamed to something like 'counterSideVolumes' or 'counterSideState'. _side and _otherSide are actually different things, of different types, naming them so confuses your code reader very much
     const priceArr = {};
     let traded = 0;
     for (let i = 0; i< matches.length; i++) {
-      const { price, quantity, tradedQuantity } = matches[i];
+      const { price, quantity, tradedQuantity } = matches[i]; // [Tung]: also destruct the 'filledCompletely' field of each match object
       const priceStr = price.toString();
       traded += tradedQuantity;
       if (typeof priceArr[priceStr] === 'undefined') priceArr[priceStr] = { quantity, tradedQuantity };
